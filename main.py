cc386fd64f8aa359e29bbaf04c46effee504a8db
tfarnecim <36956453+tfarnecim@users.noreply.github.com>
2020-12-09 13:02:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def Cadastro(cpf,email,senha):
 	if(cpf=="" or email=="" or senha==""):
 		return 2
 
-	con = psycopg2.connect(database='Cinema', user='postgres', password='12345')
+	con = psycopg2.connect(database='Cinema', user='postgres', password='postgres')
 	cur = con.cursor()
-	cur.execute('select cpf from usuario where cpf=%s'%(cpf))
+	cur.execute("select cpf from usuario where cpf='%s';"%(cpf))
 	l = cur.fetchall()
 	con.close()
 
@@ -64,9 +64,10 @@ def Cadastro(cpf,email,senha):
 	if(p==0 or p==len(email)-1):
 		return 5
 
-	con = psycopg2.connect(database='Cinema', user='postgres', password='12345')
+	con = psycopg2.connect(database='Cinema', user='postgres', password='postgres')
 	cur = con.cursor()
-	cur.execute('insert into usuario values(%s,%s,%s)'%(cpf,email,senha))
+	cur.execute("insert into usuario values('%s','%s','%s');"%(cpf,email,senha))
+	con.commit()
 	con.close()
 
 	return 1
@@ -77,9 +78,9 @@ def Login(Cpf, Senha):
 	#2 Cpf não cadastrado
 	#3 Senha errada
 
-	con = psycopg2.connect(database='Cinema', user='postgres', password='12345')
+	con = psycopg2.connect(database='Cinema', user='postgres', password='postgres')
 	cur = con.cursor()
-	cur.execute('select cpf,senha from usuario where cpf=%s'%(Cpf))
+	cur.execute("select cpf,senha from usuario where cpf='%s';"%(Cpf))
 	l = cur.fetchall()
 	con.close()
 
@@ -102,7 +103,16 @@ while(menu1!=3):
 
 	os.system('cls')
 
-	print("CINEMA CIDADE LUZ\n\n[1]REGISTRAR\n[2]LOGIN\n[3]SAIR\n\n")
+	print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+	print("|                                 |")
+	print("|        CINEMA CIDADE LUZ        |")
+	print("|                                 |")
+	print("|        [1]Registrar             |")
+	print("|        [2]Login                 |")
+	print("|        [3]Sair                  |")
+	print("|                                 |")
+	print("|                                 |")
+	print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 	menu1 = int(input())
 
 	if(menu1 == 1):#caso o usuario deseje se registrar
@@ -118,6 +128,8 @@ while(menu1!=3):
 			"Email inválido",
 			"CPF já cadastrado"
 		]
+		
+		volta = False
 
 		while(menu2 != 1):
 
@@ -126,14 +138,24 @@ while(menu1!=3):
 			senha = ""
 
 			os.system("cls")
-			cpf = input("CPF: ")
-			email = input("EMAIL: ")
-			senha = input("SENHA: ")
+			cpf = input("Digite seu CPF: ")
+			email = input("Digite seu EMAIL: ")
+			senha = input("Digite sua nova SENHA: ")
 
 			menu2 = Cadastro(cpf,email,senha)
 			print("\nLOG: %s"%(Log[menu2]))
-			os.system("pause")
 
+			if(menu2!=1):
+				print("\nDeseja retornar ao menu principal?\n[1]Sim\n[2]Nao\n")
+				menu2 = int(input())
+				if(menu2==1):
+					volta = True
+					break
+			else:
+				os.system("pause")
+
+		if(volta == True):		
+			continue
 
 
 	if(menu1 == 2):#caso o usuario deseje logar
@@ -147,22 +169,48 @@ while(menu1!=3):
 			"Senha errada"
 		]
 
+		volta = False 
+
 		while(menu2 != 1):
 
 			cpf = ""
 			senha = ""
 
 			os.system("cls")
-			cpf = input("CPF: ")			
-			senha = input("SENHA: ")
+
+			cpf = input("Digite seu CPF: ")			
+			senha = input("Digite sua SENHA: ")
 
 			menu2 = Login(cpf,senha)
+			
 			print("\nLOG: %s"%(Log[menu2]))
-			os.system("pause")
+
+			if(menu2!=1):
+				print("\nDeseja retornar ao menu principal?\n[1]Sim\n[2]Nao\n")
+				menu2 = int(input())
+				if(menu2==1):
+					volta = True
+			else:
+				os.system("pause")
 
 		os.system("cls")
 
-		print("IMAGINE UMA TELA PRINCIPAL \nDO USUARIO MUITO FODA AQUI\nTU TEM QUE FECHAR O PROGRAMA\nPQ ELE PARA AQUI\nEU NAO TENHO ESSE TEMPO TODO PRA \nPROGRAMAR TUDO EM UM DIA NAO MEUAMIGO")
-		
+		if(volta == True):
+			continue
+
+		print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+		print("|                                 |")
+		print("|        CINEMA CIDADE LUZ        |")
+		print("|                                 |")
+		print("|        [1]Comprar filmes        |")
+		print("|        [2]Usar bilhete          |")
+		print("|        [3]Voltar                |")
+		print("|                                 |")
+		print("|                                 |")
+		print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+		menu1 = int(input())
+
 		while(1):
 			pass
+
+os.system("cls")
